40650613d689b9abfac1717e4d165d7602403a9d
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-23 22:54:19 -0300

    polices: add cors options and helmet

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerConfig } from './config/swagger/swagger.config';
+import { corsConfig } from './config/cors/cors.config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,10 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('doc', app, document); // Acesse a documentação em /doc
+
+  app.use(helmet());
+
+  app.enableCors(corsConfig);
 
   await app.listen(process.env.PORT ?? 3001);
 }
