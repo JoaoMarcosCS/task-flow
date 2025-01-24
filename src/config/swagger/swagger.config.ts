31c47aea7951215176ec4373288dcb85bfa28262
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-23 23:01:08 -0300

    refact: swagger config

--- src/config/swagger/swagger.config.ts
+++ src/config/swagger/swagger.config.ts
@@ -1,8 +1,14 @@
-import { DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Task Flow')
-  .setDescription('Documentação da API do projeeto Task Flow')
-  .setVersion('1.0')
-  .addTag('examples')
-  .build();
+export function swaggerConfig(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('Task Flow API')
+    .setDescription('Descrição da API do projeto Task Flow')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule } from '@nestjs/swagger';
-import { swaggerConfig } from './config/swagger/swagger.config';
 import { corsConfig } from './config/cors/cors.config';
 import helmet from 'helmet';
+import { swaggerConfig } from './config/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('doc', app, document); // Acesse a documentação em /doc
+  swaggerConfig(app);
 
   app.use(helmet());
 
   app.enableCors(corsConfig);
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app
+    .listen(process.env.PORT ?? 3001)
+    .then(() => console.log(`🚀 Server on: http://localhost:3001/`));
 }
 bootstrap();
