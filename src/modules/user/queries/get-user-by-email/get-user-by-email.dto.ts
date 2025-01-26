74014631bce9cfadf0444c1273bfba2fa91b5836
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-26 12:57:23 -0300

    doc: add swagger

--- src/modules/user/queries/get-user-by-email/get-user-by-email.dto.ts
+++ src/modules/user/queries/get-user-by-email/get-user-by-email.dto.ts
@@ -1,5 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class GetUserByEmailDto {
+  @ApiProperty({ description: 'ID do usuário', example: 1 })
   id: number;
-  name: number;
+
+  @ApiProperty({ description: 'Nome do usuário', example: 'João Silva' })
+  name: string;
+
+  @ApiProperty({
+    description: 'Email do usuário',
+    example: 'joao.silva@email.com',
+  })
   email: string;
 }
