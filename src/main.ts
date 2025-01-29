622bee6748d586303b556b1f987f0bac77884972
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-29 11:31:54 -0300

    fix: add link to swagger doc

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
 
   await app
     .listen(process.env.PORT ?? 3001)
-    .then(() => console.log(`🚀 Server on: http://localhost:3001/`));
+    .then(() => console.log(`🚀 Server on: http://localhost:3001/api/docs`));
 }
 bootstrap();
