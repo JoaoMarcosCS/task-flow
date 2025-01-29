660ac6b6c4391d9d34ceac243739d91814eff5d2
João Marcos <jmcsjoaomarcos@gmail.com>
2025-01-29 18:25:50 -0300

    feat: get tasks by user id

---
+++ src/modules/task/queries/get-tasks-by-user-id/get-tasks-by-user-id.handler.ts
@@ -0,0 +1,40 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetTasksByUserIdQuery } from './get-tasks-by-user-id.query';
+import { GetTasksByUserIdDto } from './get-tasks-by-user-id.dto';
+import { DataSource } from 'typeorm';
+import { Task } from '../../entities/task.entity';
+
+@QueryHandler(GetTasksByUserIdQuery)
+export class GetTasksByUserIdHandler
+  implements IQueryHandler<GetTasksByUserIdQuery, GetTasksByUserIdDto>
+{
+  constructor(private readonly dataSource: DataSource) {}
+
+  async execute(query: GetTasksByUserIdQuery): Promise<GetTasksByUserIdDto> {
+    const tasks = await this.dataSource.manager.find(Task, {
+      where: {
+        assignees: {
+          id: query.userId,
+        },
+      },
+      relations: ['assignees', 'priority', 'status', 'board'],
+      select: {
+        assignees: {
+          name: true,
+          email: true,
+        },
+        board: {
+          title: true,
+        },
+        priority: {
+          description: true,
+        },
+        status: {
+          description: true,
+        },
+      },
+    });
+
+    return { tasks };
+  }
+}
---
+++ src/modules/task/queries/get-tasks-by-user-id/get-tasks-by-user-id.query.ts
@@ -0,0 +1,3 @@
+export class GetTasksByUserIdQuery {
+  userId: number;
+}
